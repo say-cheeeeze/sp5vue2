90867a50945afd7c47cf81e96862dcf6b41c8b0c
cheeeeze <nyj9349@hanmail.net>
2021-08-18 22:45:12 +0900

    add

--- chapter3Spring/src/main/java/app/messages/AppConfig.java
+++ chapter3Spring/src/main/java/app/messages/AppConfig.java
@@ -1,6 +1,5 @@
 package app.messages;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,13 +8,5 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan("app.messages")
 public class AppConfig {
     
-    @Bean
-    public MessageRepository messageRepository() {
-        return new MessageRepository();
-    }
-
-    @Bean
-    MessageService messageService() {
-        return new MessageService( messageRepository() );
-    }
+    
 }
