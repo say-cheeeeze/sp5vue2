a35eec6b620c580ef8758363fee0f9054d6edf39
cheeeeze <nyj9349@hanmail.net>
2021-08-31 23:40:53 +0900

    add

--- chapter3Spring/src/main/java/app/messages/AppConfig.java
+++ chapter3Spring/src/main/java/app/messages/AppConfig.java
@@ -2,11 +2,20 @@ package app.messages;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Bean;
 
 // 스프링이 컨테이너를 인스턴스화하는데 사용할 설정 메타 데이터
 @Configuration
 @ComponentScan("app.messages")
 public class AppConfig {
     
-    
+    @Bean
+    public MessageRepository messageRepository() {
+        return new MessageRepository();
+    }
+
+    @Bean
+    MessageService messageService() {
+        return new MessageService ( messageRepository() );
+    }
 }
--- chapter3Spring/src/main/java/app/messages/Message.java
+++ chapter3Spring/src/main/java/app/messages/Message.java
@@ -4,6 +4,7 @@ package app.messages;
  * 메시지 객체
  */
 public class Message {
+    
     private String text;
     public Message( String text ) {
         this.text = text;
